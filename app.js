7c9b8abe3126687f995daace3566649b3557033f
Script-Demon <tanveermahmud0@gmail.com>
2024-05-13 00:12:48 -0700

    practice start

---
+++ app.js
@@ -0,0 +1,6 @@
+const number1 = 10;
+const number2 = 20;
+
+if (number1<number2) {
+    console.log(false);
+}
---
+++ operator.js
@@ -0,0 +1,9 @@
+// javascript operator
+
+// // 6 type of js operator
+// 1.arithmatic operator -> (+, -, /, %, ++, --)
+// 2.comparison operator -> (=, ==, ===, !, !=, !==, <=, >=, <, >)
+// 3.assignment operator -> (=, +=, -= )
+// 4.logical operator -> (&&, ||, !)
+// 5.bitwise operator
+// 6.special operator
