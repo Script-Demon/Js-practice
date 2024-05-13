bcbdefe734c88e38e6591c0bc3cb2867d4ec5c05
Script-Demon <tanveermahmud0@gmail.com>
2024-05-13 09:33:01 -0700

    math function

--- app.js
+++
@@ -1,9 +0,0 @@
-const number1 = 10;
-const number2 = 20;
-
-if (number1>number2) {
-    console.log(false);
-}
-else {
-    console.log(true);
-}
---
+++ math function.js
@@ -0,0 +1,11 @@
+// console.log(Math.PI);
+
+// let n = 4.56782;
+
+// console.log(Math.abs(n));
+// console.log(Math.floor(n));
+// console.log(Math.ceil(n));
+// console.log(Math.pow(2,3));
+
+// console.log(Math. Random());
+// console.log(Math .random());
